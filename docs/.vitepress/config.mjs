6f82074eb3ccf8a2ac7b45a937499c213a1df06b
zelzmiy <51797223+zelzmiy@users.noreply.github.com>
2026-09-16 18:44:36 -0500

    feat: spans support light mode
    also change colors slightly
    also kill me, there has to be a better way to do that i'm just kinda bad at CSS

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from "vitepress";
 import gmlGrammar from "./gml.tmLanguage.json" with { type: "json" };
 import gmlThemeDark from "./gml.themeDark.json" with { type: "json" };
-import gmlThemeLight from "./gml.themeLight.json" with  { type: "json "};
+import gmlThemeLight from "./gml.themeLight.json" with  { type: "json"};
 
 import tlfGrammar from "./tlf.tmLanguage.json" with { type: "json" };
 
